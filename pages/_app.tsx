c78b76d479ffd54a26b1525f19a629eed1eef129
Sweet4ever <oskpil@gmail.com>
2022-09-16 08:22:26 +0200

    added vcc-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
-import { HelloWorld } from "../src/components/HelloWorld";
-import "../public/css/styles.css";
+import { CarList } from "../src/components/carList";
 import React from "react";
+import { StyleProvider, ThemePicker } from "vcc-ui";
 
 function HomePage() {
   return (
     <React.StrictMode>
-      <HelloWorld />
+      <StyleProvider>
+        <ThemePicker variant="light">
+          <CarList />
+        </ThemePicker>
+      </StyleProvider>
     </React.StrictMode>
   );
 }
