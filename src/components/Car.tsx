da6eb5b828f889ec1066e2e1452db68cbddf46d4
Sweet4ever <oskpil@gmail.com>
2022-09-16 12:02:09 +0200

    styling landing page

--- src/components/Car.tsx
+++ src/components/Car.tsx
@@ -1,17 +1,48 @@
 import React from "react";
-import { Block } from "vcc-ui";
+import { Block, Link } from "vcc-ui";
 import CarInterface from "../Interfaces/CarInterface";
+import Image from "next/image";
 
 export const Car: React.FC<{ car: CarInterface }> = ({ car }) => {
   return (
-    <Block>
+    <Block
+      extend={{
+        margin: "1vw",
+        width: "280px",
+        height: "500px",
+      }}
+    >
       <h2>{car.bodyType}</h2>
       <h1>{car.modelName}</h1>
       <h2>{car.modelType}</h2>
-      <img src={car.imageUrl} alt={car.id} />
-      <Block>
-        <a href="">LEARN</a>
-        <a href="">SHOP</a>
+      <Image
+        src={car.imageUrl}
+        alt={car.id}
+        width={800}
+        height={600}
+        layout="responsive"
+      />
+      <Block
+        extend={{
+          display: "flex",
+          "justify-content": "space-around",
+          "align-items": "center",
+          marginTop: "4vh",
+        }}
+      >
+        <Block
+          extend={{
+            display: "flex",
+            "align-items": "center",
+          }}
+        >
+          <Link href="https://www.volvocars.com/" arrow="right">
+            LEARN
+          </Link>
+        </Block>
+        <Link href="https://www.volvocars.com/" arrow="right">
+          SHOP
+        </Link>
       </Block>
     </Block>
   );
--- src/components/carList.tsx
+++ src/components/carList.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Block } from "vcc-ui";
+import { Flex, Block, Icon } from "vcc-ui";
 import { Car } from "./Car";
 import data from "../../public/api/cars.json";
 import CarInterface from "../Interfaces/CarInterface";
@@ -8,10 +8,46 @@ export const CarList: React.FC = () => {
   const newData: CarInterface[] = [...data];
 
   return (
-    <Block>
-      {newData.map((data) => (
-        <Car key={data.id} car={data}></Car>
-      ))}
-    </Block>
+    <Flex
+      id="hej"
+      extend={{
+        "flex-direction": "column",
+        "align-items": "center",
+        "align-self": "auto",
+      }}
+    >
+      <Block
+        extend={{
+          display: "flex",
+          "flex-direction": "column",
+          "justify-content": "center",
+          width: "80vw",
+        }}
+      >
+        <Flex
+          extend={{
+            height: "500px",
+            "flex-wrap": "wrap",
+            "flex-direction": "row",
+            "justify-content": "center",
+            overflow: "hidden",
+          }}
+        >
+          {newData.map((data) => (
+            <Car key={data.id} car={data}></Car>
+          ))}
+        </Flex>
+        <Flex
+          extend={{
+            "flex-direction": "row",
+            "justify-content": "flex-end",
+            paddingRight: "58.5px",
+          }}
+        >
+          <Icon type="mediacircled-previous-32" />
+          <Icon type="mediacircled-next-32" />
+        </Flex>
+      </Block>
+    </Flex>
   );
 };
