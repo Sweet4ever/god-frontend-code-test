7e8b4cfb9048ec852752e2b4babba2efe960a7e6
Sweet4ever <oskpil@gmail.com>
2022-09-19 08:53:20 +0200

    design for desktop

--- src/components/carList.tsx
+++ src/components/carList.tsx
@@ -1,15 +1,40 @@
-import React from "react";
-import { Flex, Block, Icon } from "vcc-ui";
+import React, { useState } from "react";
+import { Flex, Block, Icon, Click } from "vcc-ui";
 import { Car } from "./Car";
 import data from "../../public/api/cars.json";
 import CarInterface from "../Interfaces/CarInterface";
 
 export const CarList: React.FC = () => {
   const newData: CarInterface[] = [...data];
+  let carChunks: CarInterface[][] = [];
+  let chunk: CarInterface[] = [];
+  let [slide, setSlide] = useState(0);
+
+  for (let i = 0; i < newData.length; i++) {
+    chunk.push(newData[i]);
+    if (chunk.length === 4) {
+      carChunks.push(chunk);
+      chunk = [];
+    }
+  }
+  const prevPage = () => {
+    if (slide - 1 < 0) {
+      setSlide(carChunks.length - 1);
+    } else {
+      setSlide(slide - 1);
+    }
+  };
+  const nextPage = () => {
+    if (slide + 1 >= carChunks.length) {
+      setSlide(0);
+    } else {
+      setSlide(slide + 1);
+    }
+  };
 
   return (
     <Flex
-      id="hej"
+      id="1"
       extend={{
         "flex-direction": "column",
         "align-items": "center",
@@ -17,6 +42,7 @@ export const CarList: React.FC = () => {
       }}
     >
       <Block
+        id="2"
         extend={{
           display: "flex",
           "flex-direction": "column",
@@ -25,6 +51,7 @@ export const CarList: React.FC = () => {
         }}
       >
         <Flex
+          id="3"
           extend={{
             height: "500px",
             "flex-wrap": "wrap",
@@ -33,19 +60,24 @@ export const CarList: React.FC = () => {
             overflow: "hidden",
           }}
         >
-          {newData.map((data) => (
-            <Car key={data.id} car={data}></Car>
-          ))}
+          {carChunks[slide].map((data) => {
+            return <Car car={data} key={data.id}></Car>;
+          })}
         </Flex>
         <Flex
+          id="4"
           extend={{
             "flex-direction": "row",
             "justify-content": "flex-end",
             paddingRight: "58.5px",
           }}
         >
-          <Icon type="mediacircled-previous-32" />
-          <Icon type="mediacircled-next-32" />
+          <Click onClick={prevPage}>
+            <Icon type="mediacircled-previous-32" />
+          </Click>
+          <Click onClick={nextPage}>
+            <Icon type="mediacircled-next-32" />
+          </Click>
         </Flex>
       </Block>
     </Flex>
